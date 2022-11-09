732d9fd5174fcf6ac3d7448e3358a184ab141c3d
ShannaSmith <shannagjh@gmail.com>
2022-11-09 00:11:58 -0500

    add comments

--- sales_report.py
+++ sales_report.py
@@ -1,25 +1,24 @@
 """Generate sales report showing total melons each salesperson sold."""
 
 
-salespeople = []
-melons_sold = []
+salespeople = [] #<===initalizes list of salespeople
+melons_sold = [] #<===initalizes list of number of melons sold by each salesperson
 
-f = open('sales-report.txt')
+f = open('sales-report.txt') #<=== opens the sales report file that conatins details about recent orders
 
-for line in f:
-    line = line.rstrip()
-    entries = line.split('|')
-    salesperson = entries[0]
-    melons = int(entries[2])
-
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
+for line in f:  #<==begins for loop
+    line = line.rstrip() #<=== removes trailing white space
+    entries = line.split('|') #<===returns a list and separates the name of the salesperson , total amount of order, 
+    #and total amount of melons sold into seperate strings for each line of data
+    salesperson = entries[0] #<== assigns data at index 0 to salesperson. This is the name of the salesperson
+    melons = int(entries[2])#<==assigns data at index 2 to melons. This is the total amt of melons sold.
+    if salesperson in salespeople: #<===initialiizes if statement (conditional). Checks if the salesperson is already in the list
+        position = salespeople.index(salesperson)#<===assigns a specific
         melons_sold[position] += melons
-        
     else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
+        salespeople.append(salesperson) #<===add salesperson to the salespeople list
+        melons_sold.append(melons) #<===add melon sold count to melons list
 
 
-for i in range(len(salespeople)):
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+for i in range(len(salespeople)): #<===initialize for statement to run through the salespeople list
+    print(f'{salespeople[i]} sold {melons_sold[i]} melons') #<===print a formatted literal showing the salesperson's name,  and how many melons sold
