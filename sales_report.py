4a9fe08d0b17165a64d7c449fdff23a287ab1a89
ShannaSmith <shannagjh@gmail.com>
2022-11-09 00:20:22 -0500

    add to comments

--- sales_report.py
+++ sales_report.py
@@ -13,12 +13,12 @@ for line in f:  #<==begins for loop
     salesperson = entries[0] #<== assigns data at index 0 to salesperson. This is the name of the salesperson
     melons = int(entries[2])#<==assigns data at index 2 to melons. This is the total amt of melons sold.
     if salesperson in salespeople: #<===initialiizes if statement (conditional). Checks if the salesperson is already in the list
-        position = salespeople.index(salesperson)#<===assigns a specific
-        melons_sold[position] += melons
+        position = salespeople.index(salesperson)#<===find the position where salesperson is stored in salespeople
+        melons_sold[position] += melons #???
     else:
         salespeople.append(salesperson) #<===add salesperson to the salespeople list
         melons_sold.append(melons) #<===add melon sold count to melons list
 
 
-for i in range(len(salespeople)): #<===initialize for statement to run through the salespeople list
+for i in range(len(salespeople)): #<===initialize for statement to run through the salespeople list, loop over indiices of salespeople and use it to index into salespeople and melons_sold
     print(f'{salespeople[i]} sold {melons_sold[i]} melons') #<===print a formatted literal showing the salesperson's name,  and how many melons sold
